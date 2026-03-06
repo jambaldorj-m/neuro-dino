e24059630fe2a7cbd14402e04396855dadaf2f2c
tiss-asleep <jambalm0674@gmail.com>
2026-03-06 12:10:34 -0500

    Added jump() function to update vertical velocity and is_jumping bool of dinosaur to dino.py

--- game/dino.py
+++ game/dino.py
@@ -4,14 +4,25 @@ DINO_WIDTH = 40
 DINO_HEIGHT = 50
 DINO_COLOR = (53, 53, 53)
 
+GRAVITY = 1.2
+JUMP_VELOCITY = -18
+
 class Dino:
-    __slots__ = ["x", "y", "width", "height"]
+    __slots__ = ["x", "y", "width", "height", "ground_y", "velocity_y", "is_jumping"]
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.width = DINO_WIDTH
         self.height = DINO_HEIGHT
+        self.ground_y = y
+        self.velocity_y = 0
+        self.is_jumping = False
+
+    def jump(self):
+        if not self.is_jumping:
+            self.velocity_y = JUMP_VELOCITY
+            self.is_jumping = True
 
     def draw(self, screen):
         pygame.draw.rect(screen, DINO_COLOR, (self.x, self.y, self.width, self.height))
