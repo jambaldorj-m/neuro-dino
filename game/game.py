a1eb22f80a9a0c503d78f9e20ade4f98ccd999ae
tiss-asleep <jambalm0674@gmail.com>
2026-03-06 12:04:08 -0500

    Added the test dino object into game.py

--- game/game.py
+++ game/game.py
@@ -1,16 +1,21 @@
 import pygame
 import sys
+from dino import Dino, DINO_HEIGHT
 
 SCREEN_WIDTH = 900
-SCREEN_HEIGHT = 400
+SCREEN_HEIGHT = 300
 FPS = 60
 BG_COLOR = (255, 255, 255)
+GROUND_HEIGHT = 250
+GROUND_COLOR = (53, 53, 53)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Neuro Dino")
     clock = pygame.time.Clock()
+    
+    dino = Dino(x=80, y=GROUND_HEIGHT - DINO_HEIGHT)
 
     running = True
     while running:
@@ -22,6 +27,8 @@ def main():
                     running = False
 
         screen.fill(BG_COLOR)
+        pygame.draw.line(screen, GROUND_COLOR, (0, GROUND_HEIGHT), (SCREEN_WIDTH, GROUND_HEIGHT), 2)
+        dino.draw(screen)
         pygame.display.flip()
 
         clock.tick(FPS)
