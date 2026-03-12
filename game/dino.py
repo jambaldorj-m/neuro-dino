56007060e09e3eaa5a38c672b56e1d5c69e5f1a9
tiss-asleep <jambalm0674@gmail.com>
2026-03-11 23:07:40 -0400

    added methods relating to ducking in dino.py

--- game/dino.py
+++ game/dino.py
@@ -2,34 +2,50 @@ import pygame
 
 DINO_WIDTH = 40
 DINO_HEIGHT = 50
+DINO_DUCK_WIDTH = 60
+DINO_DUCK_HEIGHT = 25
 DINO_COLOR = (53, 53, 53)
-
 GRAVITY = 1.2
 JUMP_VELOCITY = -18
 
 class Dino:
-    __slots__ = ["x", "y", "width", "height", "ground_y", "velocity_y", "is_jumping"]
+    __slots__ = ["x", "y", "width", "height", "ground_y", "velocity_y", "is_jumping", "is_ducking"]
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.width = DINO_WIDTH
         self.height = DINO_HEIGHT
-        self.ground_y = y
+        self.ground_y = y + DINO_HEIGHT # store the raw ground line
         self.velocity_y = 0
         self.is_jumping = False
+        self.is_ducking = False
 
     def jump(self):
         if not self.is_jumping:
             self.velocity_y = JUMP_VELOCITY
             self.is_jumping = True
 
+    def duck(self):
+        if not self.is_jumping:
+            self.is_ducking = True
+            self.width = DINO_DUCK_WIDTH
+            self.height = DINO_DUCK_HEIGHT
+            self.y = self.ground_y - self.height
+
+    def stop_duck(self):
+        if self.is_ducking:
+            self.is_ducking = False
+            self.width = DINO_WIDTH
+            self.height = DINO_HEIGHT
+            self.y = self.ground_y - self.height
+
     def update(self):
         self.velocity_y += GRAVITY
         self.y += self.velocity_y
 
-        if self.y >= self.ground_y:
-            self.y = self.ground_y
+        if self.y >= self.ground_y - self.height:
+            self.y = self.ground_y - self.height
             self.velocity_y = 0
             self.is_jumping = False
 
